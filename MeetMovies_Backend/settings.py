0291e5d6a2db57a3b8e9704fdb726d5875eaa126
scongl <sicongli123@163.com>
2022-12-21 22:29:23 +0800

    关闭debug

--- MeetMovies_Backend/settings.py
+++ MeetMovies_Backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8^+&j^txqob(pxg-%rt$7)uhk=k3@qvpfmri8xpkb9w^9hfu9x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
